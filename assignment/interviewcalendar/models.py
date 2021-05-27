553a102e0941ba809376d972afbf87358b84d96d
dylodylo <artur.dylewski.27@gmail.com>
2021-05-27 18:56:24 +0100

    Change User model
    Change slots field in User model

--- assignment/interviewcalendar/models.py
+++ assignment/interviewcalendar/models.py
@@ -3,4 +3,4 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     is_candidate = models.BooleanField(default=True)
-    slots = models.JSONField(default=dict)
+    slots = models.JSONField(default=dict, blank=True)

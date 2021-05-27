879e6479f9433a5799cf83406b87fa2933e91cb4
dylodylo <artur.dylewski.27@gmail.com>
2021-05-27 19:54:29 +0100

    Add User PUT view

--- assignment/interviewcalendar/urls.py
+++ assignment/interviewcalendar/urls.py
@@ -3,7 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('snippets/', views.UserList.as_view()),
+    path('users/<int:pk>', views.UserUpdate.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- assignment/interviewcalendar/views.py
+++ assignment/interviewcalendar/views.py
@@ -1,39 +1,7 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from .models import User
 from .serializers import UserSerializer
-from rest_framework import status
-
-# @api_view(['GET'])
-# def users_list(request, format=None):
-#     """
-#     List all code snippets, or create a new snippet.
-#     """
-#     if request.method == 'GET':
-#         users = User.objects.all()
-#         serializer = UserSerializer(users, many=True)
-#         return Response(serializer.data)
-
-
-# @api_view(['PUT'])
-# def add_slot(request, pk):
-#     """
-#     Add new free slot for user.
-#     """
-#     try:
-#         user = User.objects.get(pk=pk)
-#     except User.DoesNotExist:
-#         return Response(status=status.HTTP_404_NOT_FOUND)
-#     if request.method == 'PUT':
-#         serializer = UserSerializer(user, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 from rest_framework import generics
 
-
-class UserList(generics.ListCreateAPIView):
+class UserUpdate(generics.UpdateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer

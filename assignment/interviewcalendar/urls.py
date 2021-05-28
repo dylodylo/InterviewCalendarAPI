6bb094787c52230c7c420ebf2a795a6457017838
dylodylo <artur.dylewski.27@gmail.com>
2021-05-28 16:46:33 +0100

    Implement dates_list view
    Dates_list function return dictionary with periods of time when it's possible to arrange an interview.

--- assignment/interviewcalendar/urls.py
+++ assignment/interviewcalendar/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
     path('users/<int:pk>', views.UserUpdate.as_view()),
+    re_path(r'^users/(?P<pk>[0-9]+)/dates/$', views.dates_list),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- assignment/interviewcalendar/views.py
+++ assignment/interviewcalendar/views.py
@@ -1,7 +1,31 @@
 from .models import User
 from .serializers import UserSerializer
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 class UserUpdate(generics.UpdateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+@api_view(['GET'])    
+def dates_list(request, pk):
+    if request.method == 'GET':
+        candidate = User.objects.get(pk=pk)
+        candidate_slots = candidate.slots
+        recruiters_ids = request.query_params.get('r_ids')  # u'2,3,4' <- this is unicode
+        recruiters_ids = recruiters_ids.split(',')
+        recruiters = User.objects.filter(pk__in=recruiters_ids)
+
+        for recruiter in recruiters:
+            recruiter_slots = recruiter.slots
+            for date in candidate_slots:
+                if date not in recruiter_slots:
+                    del candidate_slots[date]
+                else:
+                    candidate_hours = candidate_slots[date]
+                    recruiter_hours = recruiter_slots[date]
+                    candidate_slots[date] = list(set(candidate_hours) & set(recruiter_hours))
+
+        return Response(candidate_slots)

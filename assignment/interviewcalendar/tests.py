20441b2f997c17ec61ef8bfd1c0dca4161b87636
dylodylo <artur.dylewski.27@gmail.com>
2021-06-01 16:54:30 +0100

    Edit tests to deal with new date format

--- assignment/interviewcalendar/tests.py
+++ assignment/interviewcalendar/tests.py
@@ -13,13 +13,13 @@ class UserModelTest(TestCase):
     """
 
     def setUp(self) -> None:
-        self.testuser1 = User.objects.create_user(username='TestUser1', password='password', slots={"20-06-2021": [ 4, 5, 6 ]})
-        self.testuser2 = User.objects.create_user(username='TestUser2', password='password', slots={"20-06-2021": [ 6, 7, 8 ]})
-        self.testuser3 = User.objects.create_user(username='TestUser3', password='password', slots={"20-06-2021": [ 1, 6, 8 ]})
-        self.valid_slots = {"22-06-2021": [ 1, 2, 3 ]}
-        self.valid_added_slots = {**self.testuser1.slots, **self.valid_slots} #combine two dicts
+        self.testuser1 = User.objects.create_user(username='TestUser1', password='password', slots={"slots":["20/06/2021 12:00-13:00", "20/06/2021 13:00-14:00"]})
+        self.testuser2 = User.objects.create_user(username='TestUser2', password='password', slots={"slots":["20/06/2021 12:00-13:00", "20/06/2021 14:00-15:00"]})
+        self.testuser3 = User.objects.create_user(username='TestUser3', password='password', slots={"slots":["20/06/2021 12:00-13:00", "20/06/2021 15:00-16:00"]})
+        self.valid_slots = {"slots":["20/06/2021 11:00-12:00"]}
+        self.valid_added_slots = {"slots":["20/06/2021 12:00-13:00", "20/06/2021 13:00-14:00", "20/06/2021 11:00-12:00"]} #combine two dicts
         self.client = APIClient()
-        self.valid_slotes_returned = {"20-06-2021": [ 6 ]}
+        self.valid_slotes_returned = ["20/06/2021 12:00-13:00"]
 
     def test_user_creation(self):
         users_count = len(User.objects.all())
@@ -34,34 +34,27 @@ class UserModelTest(TestCase):
         self.client.login(username=self.testuser1.username, password='password')
         response = self.client.patch(f'/users/{self.testuser1.id}', data=json.dumps({"slots": self.valid_slots}), content_type='application/json')
         self.testuser1.refresh_from_db()
-        self.assertEqual(self.valid_added_slots, self.testuser1.slots)
+        self.assertEqual(self.valid_added_slots['slots'].sort(), self.testuser1.slots['slots'].sort())
         self.client.logout()
-
-    def test_invalid_key_patch(self):
-        self.client.login(username=self.testuser1.username, password='password')
-        url = f'/users/{self.testuser1.id}'
-        invalid_key = {"20-20-20": [ 1, 6, 7 ]}
-        response = self.client.patch(url, data=json.dumps({"slots": invalid_key}), content_type='application/json')
-        self.assertNotEqual(response.status_code, status.HTTP_200_OK)
 
     def test_invalid_value_patch(self):
         self.client.login(username=self.testuser1.username, password='password')
         url = f'/users/{self.testuser1.id}'
-        invalid_value = {"20-02-2022": "hours"}
+        invalid_value = ["20/06/2021 11:00-12:00"]
         response = self.client.patch(url, data=json.dumps({"slots": invalid_value}), content_type='application/json')
         self.assertNotEqual(response.status_code, status.HTTP_200_OK)
 
     def test_invalid_date_patch(self):
         self.client.login(username=self.testuser1.username, password='password')
         url = f'/users/{self.testuser1.id}'
-        invalid_date = {"20-02-2019": [ 1, 6, 7 ]}
+        invalid_date = ["20/06/21 11:00-12:00"]
         response = self.client.patch(url, data=json.dumps({"slots": invalid_date}), content_type='application/json')
         self.assertNotEqual(response.status_code, status.HTTP_200_OK)
 
     def test_invalid_hours_patch(self):
         self.client.login(username=self.testuser1.username, password='password')
         url = f'/users/{self.testuser1.id}'
-        invalid_hours = {"20-02-2022": [ -1, 6, 7 ]}
+        invalid_hours = ["20/06/2021 11:30-12:00"]
         response = self.client.patch(url, data=json.dumps({"slots": invalid_hours}), content_type='application/json')
         self.assertNotEqual(response.status_code, status.HTTP_200_OK)
 

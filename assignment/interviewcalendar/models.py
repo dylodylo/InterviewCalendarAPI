d90889bd3ae37561db5327713946076de88d9168
dylodylo <artur.dylewski.27@gmail.com>
2021-06-01 17:29:18 +0100

    Create get_default_dict function
    Create get_default_dict function to return defualt dict to slots field in User model.

--- assignment/interviewcalendar/models.py
+++ assignment/interviewcalendar/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+def get_default_dict():
+    return {'slots': []}
+
 class User(AbstractUser):
     is_candidate = models.BooleanField(default=True)
-    slots = models.JSONField(default={'slots': []})
+    slots = models.JSONField(default=get_default_dict)

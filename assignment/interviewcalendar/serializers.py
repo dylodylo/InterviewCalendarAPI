8ec0c8fd3fb432543a99f9af482f3a952165b138
dylodylo <artur.dylewski.27@gmail.com>
2021-06-01 22:10:59 +0100

    Edit validate_slots in UserSerializer
    Improve exceptions structure and add raise error when date is before now.

--- assignment/interviewcalendar/serializers.py
+++ assignment/interviewcalendar/serializers.py
@@ -12,24 +12,28 @@ class UserSerializer(serializers.ModelSerializer):
         user = self.instance
         user_slots = user.slots['slots']     
         try:
-            for slot in value['slots']:
-                dates=slot.split(' ')
-                try:
-                    date = datetime.strptime(dates[0], '%d/%m/%Y')
-                except:
-                    raise serializers.ValidationError("Date format should be: dd/mm/yyyy")
-                try:
-                    hours = dates[1].split('-')
-                    start_hour = dt.time.fromisoformat(hours[0])
-                    end_hour = dt.time.fromisoformat(hours[1])
-                    date1 = datetime.combine(date, start_hour)
-                    date2 = datetime.combine(date, end_hour)
-                except:
-                    raise serializers.ValidationError("Hours should be in format: HH:MM-HH:MM")
-                if date2-date1 != dt.timedelta(hours=1) or date1.minute != 0:
-                    raise serializers.ValidationError("Slots can be only one hour periods, starts from full hour")
-                user_slots.append(slot)
+            slots = value['slots']
         except:
-            raise serializers.ValidationError("Give slots in lits")
+            raise serializers.ValidationError("Give slots in list")
+        for slot in slots:
+            dates=slot.split(' ')
+            try:
+                date = datetime.strptime(dates[0], '%d/%m/%Y')
+            except:
+                raise serializers.ValidationError("Date format should be: dd/mm/yyyy")
+            try:
+                hours = dates[1].split('-')
+                start_hour = dt.time.fromisoformat(hours[0])
+                end_hour = dt.time.fromisoformat(hours[1])
+                date1 = datetime.combine(date, start_hour)
+                date2 = datetime.combine(date, end_hour)
+            except:
+                raise serializers.ValidationError("Hours should be in format: HH:MM-HH:MM")
+            if date1 < datetime.now():
+                raise serializers.ValidationError("Date should be in future")
+            if date2-date1 != dt.timedelta(hours=1) or date1.minute != 0:
+                raise serializers.ValidationError("Slots can be only one hour periods, starts from full hour")
+            user_slots.append(slot)
+        
             
         return {'slots': list(set(user_slots))}
